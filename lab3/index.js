a28f42035d63da7519ef6dd3a1e7521b21a92a70
inescrf <ines.goulamhoussen@yahoo.com>
2023-10-05 18:59:58 +0200

    fix: add middleware

--- lab3/index.js
+++ lab3/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser'); // Importez le middleware body-parser
+
 const handles = require('./handles.js');
 const uuid = require('uuid');
 
 const port = 8080;
+
+// Utilisez body-parser pour analyser le corps des requêtes JSON
+app.use(bodyParser.json());
 
 app.use('/', handles);
 
