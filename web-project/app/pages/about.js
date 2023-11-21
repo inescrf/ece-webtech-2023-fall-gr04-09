02c40d6cfa02446dd686a2f9d32d96521ddb6fac
SialelliTeo <teo.sialelli@edu.ece.fr>
2023-11-21 15:39:35 +0100

    add project

---
+++ web-project/app/pages/about.js
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout.js'
+
+export default function Page() {
+  return (
+    <Layout
+      title="About us"
+      description="Don't be shy, drop us an email"
+      >
+      <h1 className='wt-title'>
+        About us
+      </h1>
+      <p>
+        about page to complete
+      </p>
+    </Layout>
+  )
+}
---
+++ web-project/app/pages/articles.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Layout from '../components/Layout.js'
+import RandomMealComponent from '../components/RandomMealComponent.js';
+
+const RepasLayout = () => {
+  return (
+    <Layout>
+      <div className="flex justify-center items-stretch">
+        {/* Première colonne */}
+        <div className="flex-grow p-4 h-full">
+        <h2 className="text-xl font-bold mb-4">Colonne 1</h2>
+          {[...Array(4)].map((_, index) => (
+            <RandomMealComponent key={index} />
+          ))}
+        </div>
+
+        {/* Deuxième colonne */}
+        <div className="flex-grow p-4 h-full">
+          <h2 className="text-xl font-bold mb-4">Colonne 2</h2>
+          {[...Array(4)].map((_, index) => (
+            <RandomMealComponent key={index} />
+          ))}
+        </div>
+
+        {/* Troisième colonne */}
+        <div className="flex-grow p-4 h-full">
+          <h2 className="text-xl font-bold mb-4">Colonne 3</h2>
+          {[...Array(4)].map((_, index) => (
+            <RandomMealComponent key={index} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default RepasLayout;
