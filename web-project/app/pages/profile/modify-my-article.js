50590b54204f00af629888f5cf1024bf7eb565cd
SialelliTeo <teo.sialelli@edu.ece.fr>
2023-11-28 14:36:41 +0100

    feat: deleting associated comments when deleting an article

--- web-project/app/pages/profile/modify-my-article.js
+++ web-project/app/pages/profile/modify-my-article.js
@@ -1,4 +1,3 @@
-// modify-my-article.js
 import { useRouter } from 'next/router';
 import { useContext, useState, useEffect } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
