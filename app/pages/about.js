e6564a86f918839a471e255db47f975cae42f420
inescrf <ines.goulamhoussen@yahoo.com>
2023-12-29 18:16:07 +0100

    feat: modify dark mode colors

--- app/pages/about.js
+++ app/pages/about.js
@@ -8,12 +8,12 @@ export default function Page() {
       <h1 className="text-3xl font-extrabold text-green-1 mb-4 text-center">About us</h1>
 
       <div className="container mx-auto p-8 max-w-3xl">
-        <p className="text-center text-gray-700 mb-6">
+        <p className="text-center text-gray-700 mb-6 dark:text-white">
           We are a team of three engineering students who are passionate about food and find it endlessly fascinating to explore flavors from around the world. 
           The combination of tastes in cooking is infinite, which is why we decided to create a website that brings together all kinds of recipes.
         </p>
         <br /> <br />
-        <div className="grid grid-cols-3 gap-4 text-center">
+        <div className="grid grid-cols-3 gap-4 text-center dark:text-white">
           <div>
             <img src="/hugo.jpeg" alt="Hugo Dalmasso" className="mx-auto h-40 w-40 rounded-full" />
             <p className="font-semibold mt-2">Hugo Dalmasso</p>
